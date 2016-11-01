9c1b08e90dd99cceaf7a6b0ff4f33821b4125d0f
Jack Mocherman <mochermanjack@gmail.com>
2016-11-01 15:09:35 -0500

    removed repl code for core.cljs and added release.clj to run production build using java -cp cljs.jar:src clojure.main release.clj command

--- out/hello_world/core.js
+++ out/hello_world/core.js
@@ -1,15 +1,5 @@
-// Compiled by ClojureScript 1.9.293 {}
+// Compiled by ClojureScript 1.9.293 {:static-fns true, :optimize-constants true}
 goog.provide('hello_world.core');
 goog.require('cljs.core');
-goog.require('clojure.browser.repl');
-if(typeof hello_world.core.conn !== 'undefined'){
-} else {
-hello_world.core.conn = clojure.browser.repl.connect.call(null,"http://localhost:9000/repl");
-}
-cljs.core.enable_console_print_BANG_.call(null);
-cljs.core.println.call(null,"Hello World from Autobuild!");
-hello_world.core.foo = (function hello_world$core$foo(a,b){
-return (a + b);
-});
-
-//# sourceMappingURL=core.js.map
+cljs.core.enable_console_print_BANG_();
+cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.array_seq(["Hello World from Autobuild!"], 0));
