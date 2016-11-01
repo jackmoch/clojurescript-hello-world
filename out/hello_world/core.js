abd5ebc8236021db9d389719ce7b63c697b969e3
Jack Mocherman <mochermanjack@gmail.com>
2016-11-01 14:44:36 -0500

    added watch.clj that runs autobuild on save using java -cp cljs.jar:src clojure.main watch.clj command

--- out/hello_world/core.js
+++ out/hello_world/core.js
@@ -2,6 +2,6 @@
 goog.provide('hello_world.core');
 goog.require('cljs.core');
 cljs.core.enable_console_print_BANG_.call(null);
-cljs.core.println.call(null,"Hello World!");
+cljs.core.println.call(null,"Hello World from Autobuild!");
 
 //# sourceMappingURL=core.js.map
